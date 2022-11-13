9598f75b937a3641edc56b6ca55b60eb8880a918
Padmanabhan <42073602+pdmnhn@users.noreply.github.com>
2022-11-13 22:06:29 +0530

    Add app bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
 import HomePage from "./Pages/HomePage";
 import ReturnPage from "./Pages/tools/ReturnPage";
 import TakePage from "./Pages/tools/TakePage";
@@ -14,25 +19,41 @@ const App = () => {
   const [authToken, setAuthToken] = useState<string | null>(null);
 
   return (
-    <Router>
-      <Routes>
-        {authToken === null ? (
-          <>
-            <Route
-              path="/"
-              element={<HomePage setAuthToken={setAuthToken} />}
-            />
-            <Route path="*" element={<Navigate replace to="/" />} />
-          </>
-        ) : (
-          <Route path="/">
-            <Route path="/" element={<ToolsPage />} />
-            <Route path="/take" element={<TakePage />} />
-            <Route path="/return" element={<ReturnPage />} />
-          </Route>
-        )}
-      </Routes>
-    </Router>
+    <>
+      <AppBar position="fixed">
+        <Toolbar
+          sx={{
+            backgroundColor: "#FEFEFE",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            variant="h6"
+            component="h1"
+            sx={{ color: "black", fontWeight: "bold" }}
+          >
+            Smart Inventory Tools & Asset Tracking
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Router>
+        <Routes>
+          {authToken === null ? (
+            <>
+              <Route index element={<HomePage setAuthToken={setAuthToken} />} />
+              <Route path="*" element={<Navigate replace to="/" />} />
+            </>
+          ) : (
+            <Route path="/">
+              <Route index element={<ToolsPage />} />
+              <Route path="/take" element={<TakePage />} />
+              <Route path="/return" element={<ReturnPage />} />
+            </Route>
+          )}
+        </Routes>
+      </Router>
+    </>
   );
 };
 
