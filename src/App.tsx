6f6d5f318bdefbe772f329b5b630fcd751c98eef
Padmanabhan <dev.pdmnhn@gmail.com>
2022-11-15 19:55:13 +0530

    Set default background for all pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import {
   Navigate,
 } from "react-router-dom";
 
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 
 import HomePage from "./Pages/HomePage";
@@ -19,41 +19,67 @@ const App = () => {
   const [authToken, setAuthToken] = useState<string | null>(null);
 
   return (
-    <>
-      <AppBar position="fixed">
-        <Toolbar
-          sx={{
-            backgroundColor: "#FEFEFE",
-            display: "flex",
-            justifyContent: "center",
-          }}
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        width: "1",
+        height: "100vh",
+      }}
+    >
+      <Paper
+        variant="outlined"
+        square
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          padding: 2,
+          background: "#0F53B7",
+        }}
+      >
+        <Typography
+          variant="h5"
+          component="h1"
+          sx={{ color: "white", fontWeight: "bold" }}
         >
-          <Typography
-            variant="h6"
-            component="h1"
-            sx={{ color: "black", fontWeight: "bold" }}
-          >
-            Smart Inventory Tools & Asset Tracking
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Router>
-        <Routes>
-          {authToken === null ? (
-            <>
-              <Route index element={<HomePage setAuthToken={setAuthToken} />} />
-              <Route path="*" element={<Navigate replace to="/" />} />
-            </>
-          ) : (
-            <Route path="/">
-              <Route index element={<ToolsPage />} />
-              <Route path="/take" element={<TakePage />} />
-              <Route path="/return" element={<ReturnPage />} />
-            </Route>
-          )}
-        </Routes>
-      </Router>
-    </>
+          Smart Inventory Tools & Asset Tracking
+        </Typography>
+      </Paper>
+      <Box sx={{ flex: 1, background: "#f5edf7" }}>
+        <Router>
+          <Routes>
+            {authToken === null ? (
+              <>
+                <Route
+                  index
+                  element={<HomePage setAuthToken={setAuthToken} />}
+                />
+                <Route path="*" element={<Navigate replace to="/" />} />
+              </>
+            ) : (
+              <Route path="/">
+                <Route index element={<ToolsPage />} />
+                <Route path="/take" element={<TakePage />} />
+                <Route path="/return" element={<ReturnPage />} />
+              </Route>
+            )}
+          </Routes>
+        </Router>
+      </Box>
+      <Paper
+        variant="outlined"
+        square
+        sx={{ display: "flex", justifyContent: "center", padding: 1 }}
+      >
+        <Typography
+          variant="body1"
+          sx={{ color: "#0F316D", fontWeight: "bold" }}
+        >
+          Developed by Ashwin, Padmanabhan and Kirithana
+        </Typography>
+      </Paper>
+    </Box>
   );
 };
 
